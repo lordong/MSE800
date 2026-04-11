fc78c506c8d92ab18732c99195911cf8ec213eb3
lordong <lordong@gmail.com>
2026-04-12 08:41:15 +1200

    Remove the end comma of each lines

--- Week1/MSE800-PSD/calc_power.py
+++ Week1/MSE800-PSD/calc_power.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-x = int(input('Please enter number x: '));
-y = int(input('Please enter number y: '));
-print('power(%d, %d) = %d' % (x, y, x ** y));
+x = int(input('Please enter number x: '))
+y = int(input('Please enter number y: '))
+print('power(%d, %d) = %d' % (x, y, x ** y))
