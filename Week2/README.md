6f3a6cf45e06b4c39e708854c06b99304c3c116e
lordong <lordong@gmail.com>
2026-04-18 09:43:31 +1200

    Add activity 1 for week 2

---
+++ Week2/README.md
@@ -0,0 +1,7 @@
+# This is an example for activity 1 of week 1
+
+1. Function cacl_sum: input two integer values and calculate their sum.
+
+2. Function calc_power: input two integer values and calculate their power.
+
+3. Function calc_complex: calculate the sum of two complex numbers.
---
+++ Week2/activity1.py
@@ -0,0 +1,29 @@
+def calc_sub():
+    try:
+        x = int(input("Please enter integer value x: "))
+        y = int(input("Please enter integer value y: "))
+        print(f"The result of {x} + {y} = ", x + y)
+    except:
+        print("Please enter integer number!")
+
+def calc_power():
+    try:
+        x = int(input("Please enter integer value x: "))
+        y = int(input("Please enter integer value y: "))
+        print(f"The result of power({x}, {y}) = ", x ** y)
+    except:
+        print("Please enter integer number!")
+
+def calc_complex():
+    x = 1 + 2j
+    y = 3 + 5j
+    z = x + y
+    print(f"Complex number: {x} + {y} = {z}, the real of z is ", z.real, ", the imag of z is ", z.imag)
+
+def main():
+    calc_sub()
+    calc_power()
+    calc_complex()
+
+if __name__ == "__main__":
+    main()
